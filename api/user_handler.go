e33007d5df746dd24709003535bb303750d945d3
hellsbells <mihalosnst4@gmail.com>
2024-02-10 12:22:31 +0200

    "First Comit"

---
+++ api/user_handler.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+	"github.com/Michalis98/hotel-reservation/types"
+	"github.com/gofiber/fiber/v2"
+)
+
+func HandleGetUsers(c *fiber.Ctx) error {
+	u := types.User{
+		FirstName: "James",
+		LastName:  "Anatasiou",
+	}
+	return c.JSON(u)
+
+}
+
+func HandleGetUser(c *fiber.Ctx) error {
+
+	return c.JSON(map[string]string{"Giannis": "Araddipou"})
+
+}
