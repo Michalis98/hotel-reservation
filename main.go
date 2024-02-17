86e3cfe2dba032f08dc6ad69f80b55a2b761e143
hellsbells <mihalosnst4@gmail.com>
2024-02-17 09:30:39 +0200

    Changes

--- main.go
+++ main.go
@@ -3,56 +3,45 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"log"
 
 	"github.com/Michalis98/hotel-reservation/api"
-	"github.com/Michalis98/hotel-reservation/types"
+	"github.com/Michalis98/hotel-reservation/db"
 	"github.com/gofiber/fiber/v2"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+// Create a new fiber instance with custom config
+var config = fiber.Config(fiber.Config{
+	// Override default error handler
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+})
+
+// ...
 
 const dburi = "mongodb://localhost:27017"
 const dbname = "hotel-reservation"
 const userColl = "users"
 
 func main() {
-	//Database stuff
+
 	//Connect to the database
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
 	if err != nil {
 		log.Fatal(err)
 	}
-	//Crate a new context
-	ctx := context.Background()
-	//Create a new database and a new collection
-	coll := client.Database(dbname).Collection(userColl)
-	//Crate a new user
-	user := types.User{
-		FirstName: "James",
-		LastName:  "Ioannou",
-	}
-	//Insert the user to the database
-	_, err = coll.InsertOne(ctx, user)
-	if err != nil {
-		log.Fatal(err)
-	}
-	//Find the user ( the query is empty because it will return the 1st occurence in the database, in our case is James)
-	var james types.User
-	if err := coll.FindOne(ctx, bson.M{}).Decode(&james); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Print(james)
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
 
 	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
 	flag.Parse()
-	app := fiber.New()
+	app := fiber.New(config)
 	apiv1 := app.Group("/api/v1")
 
-	apiv1.Get("/user", api.HandleGetUsers)
-	apiv1.Get("/user/:id", api.HandleGetUser)
+	apiv1.Get("/user", userHandler.HandleGetUsers)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
 
 	app.Listen(*listenAddr)
 
