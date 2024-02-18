39c2dcc414a273dcc11b820fd0c85ada4d1a7630
hellsbells <mihalosnst4@gmail.com>
2024-02-18 10:25:18 +0200

    -UPDATE -DELETE

--- db/user_store.go
+++ db/user_store.go
@@ -15,6 +15,8 @@ type UserStore interface {
 	GetUserByID(context.Context, string) (*types.User, error)
 	GetUsers(context.Context) ([]*types.User, error)
 	InsertUser(context.Context, *types.User) (*types.User, error)
+	DeleteUser(context.Context, string) error
+	UpdateUser(context.Context, bson.M, types.UpdateUserParams) error
 }
 
 type MongoUserStore struct {
@@ -30,6 +32,35 @@ func NewMongoUserStore(client *mongo.Client) *MongoUserStore {
 	}
 }
 
+func (s *MongoUserStore) UpdateUser(ctx context.Context, filter bson.M, params types.UpdateUserParams) error {
+
+	update := bson.D{
+		{
+			"$set", params.ToBSON(),
+		},
+	}
+	_, err := s.coll.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *MongoUserStore) DeleteUser(ctx context.Context, id string) error {
+	//validate the ID
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+	//TODO : Maybe its a good idea to handle if we did not delete any user.
+	//maybe log it or something
+	_, err = s.coll.DeleteOne(ctx, bson.M{"_id": oid})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 func (s *MongoUserStore) GetUsers(ctx context.Context) ([]*types.User, error) {
 	var users []*types.User
 	cur, err := s.coll.Find(ctx, bson.M{})
